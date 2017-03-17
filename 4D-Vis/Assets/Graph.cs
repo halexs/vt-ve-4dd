5ff5dd9766f7c026c173bd94da4e7b9098b9a780
Alex <halex@vt.edu>
2017-03-17 11:24:54 -0400

    Simple method to scale size and test unity git

--- 4D-Vis/Assets/Graph.cs
+++ 4D-Vis/Assets/Graph.cs
@@ -37,7 +37,10 @@ public class Graph : MonoBehaviour {
 
 			GameObject point = GameObject.Instantiate(datapoint_prefab, transform.position, transform.rotation);
 			point.transform.position = new Vector3 (x, y, z);
-			// todo point.render4d(t)
+
+			//Scale size by the 4th dimension.
+			point.transform.localScale += new Vector3(t,t,t);
+
 		}
 	}
 }
