2b91a55c2d6da41e74b99af956f33c2db97cdc56
lahnn <lahnn@vt.edu>
2017-03-17 12:55:51 -0400

    Added extensible 4D specfication

---
+++ 4D-Vis/Assets/Glyphs/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DataPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setPosition (Vector3 position) {
+		transform.position = position;
+	}
+
+	public abstract void setT(float t);
+}
