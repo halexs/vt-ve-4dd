2675eecac141547045c8c12f62bd646c9703c3e2
Moeti Masiane <Mmasiane@yahoo.com>
2017-03-21 11:31:46 -0400

    Add script for rotating cube around sphere

---
+++ 4D-Vis/Assets/_scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	//private GameObject sp;
+	// Use this for initialization
+	void Start () {
+		//sp = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Time.deltaTime*30, 0, 0);
+	}
+}
