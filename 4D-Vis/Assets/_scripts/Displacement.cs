ae769459e3847cf07e9c3c5f26a7194766f6ca93
Moeti Masiane <Mmasiane@yahoo.com>
2017-03-23 12:19:59 -0400

    Add sphere with child prefab

--- 4D-Vis/Assets/_scripts/Displacement.cs
+++ 4D-Vis/Assets/_scripts/Displacement.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Displacement : MonoBehaviour {
-
+	//private parentPos;
 	// Use this for initialization
 	void Start () {
-		
+		transform.localPosition = new Vector3(0,1,0);
 	}
 	
 	// Update is called once per frame
