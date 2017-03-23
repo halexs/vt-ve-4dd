bed7bbccd1eed1802bf9830c9c4655fe518ad35c
Alex <halex@vt.edu>
2017-03-23 12:01:47 -0400

    Add Colors and Opacity

---
+++ 4D-Vis/Assets/ColorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : DataPoint {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void setT(float t) {
+		//Currently sets the color to blue. Should change this whenever
+		GetComponent<Renderer> ().material.color = Color.blue;
+	}
+}
---
+++ 4D-Vis/Assets/OpacityScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpacityScript : DataPoint {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void setT(float t) {
+		//Currently sets the color to blue. Should change this whenever
+		//GetComponent<Renderer> ().material.color = t;
+
+	}
+}
